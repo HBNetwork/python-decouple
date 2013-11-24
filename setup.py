9304404030fa4118e480fc25ddca622ea0c92368
Henrique Bastos <henrique@bastos.net>
2013-11-23 23:28:31 -0200

    Bump the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 README = os.path.join(os.path.dirname(__file__), 'README.rst')
 
 setup(name='django-decouple',
-      version='1.1',
+      version='2.0',
       description='Strict separation of settings from code.',
       long_description=open(README).read(),
       author="Henrique Bastos", author_email="henrique@bastos.net",
