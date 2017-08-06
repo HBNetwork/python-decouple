0bd37e6891f60a8d6d3d840c54d8b0072ec685b5
Henrique Bastos <henrique@bastos.net>
2017-08-06 13:24:14 -0300

    Improve test_env

--- tests/test_env.py
+++ tests/test_env.py
@@ -46,38 +46,47 @@ def test_env_comment(config):
     with pytest.raises(UndefinedValueError):
         config('CommentedKey')
 
+
 def test_env_percent_not_escaped(config):
     assert '%%' == config('PercentNotEscaped')
+
 
 def test_env_no_interpolation(config):
     assert '%(KeyOff)s' == config('NoInterpolation')
 
+
 def test_env_bool_true(config):
-    assert True == config('KeyTrue', cast=bool)
-    assert True == config('KeyOne', cast=bool)
-    assert True == config('KeyYes', cast=bool)
-    assert True == config('KeyOn', cast=bool)
+    assert True is config('KeyTrue', cast=bool)
+    assert True is config('KeyOne', cast=bool)
+    assert True is config('KeyYes', cast=bool)
+    assert True is config('KeyOn', cast=bool)
+
 
 def test_env_bool_false(config):
-    assert False == config('KeyFalse', cast=bool)
-    assert False == config('KeyZero', cast=bool)
-    assert False == config('KeyNo', cast=bool)
-    assert False == config('KeyOff', cast=bool)
+    assert False is config('KeyFalse', cast=bool)
+    assert False is config('KeyZero', cast=bool)
+    assert False is config('KeyNo', cast=bool)
+    assert False is config('KeyOff', cast=bool)
+
 
 def test_env_os_environ(config):
     os.environ['KeyOverrideByEnv'] = 'This'
     assert 'This' == config('KeyOverrideByEnv')
     del os.environ['KeyOverrideByEnv']
 
+
 def test_env_undefined(config):
     with pytest.raises(UndefinedValueError):
         config('UndefinedKey')
 
+
 def test_env_default_none(config):
     assert None is config('UndefinedKey', default=None)
 
+
 def test_env_empty(config):
-    assert '' is config('KeyEmpty', default=None)
+    assert '' == config('KeyEmpty', default=None)
+
 
 def test_env_support_space(config):
     assert 'text' == config('IgnoreSpace')
--- tests/test_ini.py
+++ tests/test_ini.py
@@ -45,48 +45,60 @@ def test_ini_comment(config):
     with pytest.raises(UndefinedValueError):
         config('CommentedKey')
 
+
 def test_ini_percent_escape(config):
     assert '%' == config('PercentIsEscaped')
+
 
 def test_ini_interpolation(config):
     assert 'off' == config('Interpolation')
 
+
 def test_ini_bool_true(config):
-    assert True == config('KeyTrue', cast=bool)
-    assert True == config('KeyOne', cast=bool)
-    assert True == config('KeyYes', cast=bool)
-    assert True == config('KeyOn', cast=bool)
+    assert True is config('KeyTrue', cast=bool)
+    assert True is config('KeyOne', cast=bool)
+    assert True is config('KeyYes', cast=bool)
+    assert True is config('KeyOn', cast=bool)
+
 
 def test_ini_bool_false(config):
-    assert False == config('KeyFalse', cast=bool)
-    assert False == config('KeyZero', cast=bool)
-    assert False == config('KeyNo', cast=bool)
-    assert False == config('KeyOff', cast=bool)
+    assert False is config('KeyFalse', cast=bool)
+    assert False is config('KeyZero', cast=bool)
+    assert False is config('KeyNo', cast=bool)
+    assert False is config('KeyOff', cast=bool)
+
 
 def test_init_undefined(config):
     with pytest.raises(UndefinedValueError):
         config('UndefinedKey')
 
+
 def test_ini_default_none(config):
     assert None is config('UndefinedKey', default=None)
 
+
 def test_ini_default_bool(config):
-    assert False == config('UndefinedKey', default=False, cast=bool)
-    assert True == config('UndefinedKey', default=True, cast=bool)
+    assert False is config('UndefinedKey', default=False, cast=bool)
+    assert True is config('UndefinedKey', default=True, cast=bool)
+
 
 def test_ini_default(config):
-    assert False == config('UndefinedKey', default=False)
-    assert True == config('UndefinedKey', default=True)
+    assert False is config('UndefinedKey', default=False)
+    assert True is config('UndefinedKey', default=True)
+
 
 def test_ini_default_invalid_bool(config):
     with pytest.raises(ValueError):
         config('UndefinedKey', default='NotBool', cast=bool)
 
+
 def test_ini_empty(config):
     assert '' is config('KeyEmpty', default=None)
 
+
 def test_ini_support_space(config):
     assert 'text' == config('IgnoreSpace')
+
 
 def test_ini_os_environ(config):
     os.environ['KeyOverrideByEnv'] = 'This'
