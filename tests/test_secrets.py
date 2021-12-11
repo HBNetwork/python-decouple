e227ab62569334e4dd0d67bb4b9639f35fa1e6fb
Henrique Bastos <henrique@bastos.net>
2021-12-11 09:44:25 -0300

    Improve secret tests.

--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -12,10 +12,19 @@ def test_secrets():
     assert 'world' == config('db_password')
 
 
-def test_env_undefined_but_present_in_os_environ():
+def test_no_secret_but_present_in_os_environ():
     path = os.path.join(os.path.dirname(__file__), 'secrets')
     config = Config(RepositorySecret(path))
 
-    os.environ['KeyOnlyEnviron'] = ''
-    assert '' == config('KeyOnlyEnviron')
+    os.environ['KeyOnlyEnviron'] = 'SOMETHING'
+    assert 'SOMETHING' == config('KeyOnlyEnviron')
     del os.environ['KeyOnlyEnviron']
+
+
+def test_secret_overriden_by_environ():
+    path = os.path.join(os.path.dirname(__file__), 'secrets')
+    config = Config(RepositorySecret(path))
+
+    os.environ['db_user'] = 'hi'
+    assert 'hi' == config('db_user')
+    del os.environ['db_user']
