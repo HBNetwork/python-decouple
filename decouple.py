cc34f7edc2fae909df99282431e59cc71cabb05c
Suraj Kr Thapa <61650090+sjkrta@users.noreply.github.com>
2022-06-11 21:14:05 +1000

    Update decouple.py

--- decouple.py
+++ decouple.py
@@ -313,11 +313,11 @@ class Choices(object):
         from dj_database_url import parse as db_url
 
 # BASE_DIR, config('DEBUG'), custome name for default sqlite3 database, linking database url link
-def db_default(base_dir, debug, default_database_name, database_url):
+def db_default(debug, default_database_name, database_url):
     if debug:
         return {'default' :{
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': base_dir / f'{default_database_name}.sqlite3',
+            'NAME': default_database_name,
         }}
     else:
-        return {'default':config(database_url, cast=db_url)}
+        return {'default': database_url}
